b34ab9b092ef65dbe109ae92ecae0b360c0a3b9e
kabbajoseph <kabbajoseph072@gmail.com>
2021-02-27 08:46:18 +0300

    done

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ exports.makePayment = functions.firestore.document("/payments/{paymentId}").onCr
         const currentUUID = uuid();
 
         const { Collections } = momo.create({
-            callbackHost: "https://www.example.com/"
+            callbackHost: functions.config().momo.webhook
         });
 
         const collections = Collections({
-            userSecret: functions.config().secret,
-            userId: functions.config().id,
-            primaryKey: functions.config().key
+            userSecret: functions.config().momo.secret,
+            userId: functions.config().momo.id,
+            primaryKey: functions.config().momo.key
         });
 
         try {
 
             const payload = {
-                amount: "50000",
-                currency: "UGX",
+                amount: "11.15",
+                currency: "EUR",
                 externalId: currentUUID,
                 payer: {
                     partyIdType: "MSISDN",
@@ -43,8 +43,27 @@ exports.makePayment = functions.firestore.document("/payments/{paymentId}").onCr
 
             const response = await collections.requestToPay(payload);
 
+            const transaction = await collections.getTransaction(response);
 
-            console.log(response.transaction);
+            if (transaction.status === "SUCCESSFUL") {
+                const update = {
+                    isSuccessful: true,
+                    pending: false,
+                    financialTransactionId: transaction.financialTransactionId,
+                    response: "SUCCESSFUL"
+                };
+
+                return await admin.firestore().collection("payments").doc(`${context.params.paymentId}`).update(update);
+            } else {
+                const update = {
+                    isSuccessful: false,
+                    pending: false,
+                    response: "FAILED"
+                };
+
+                return await admin.firestore().collection("payments").doc(`${context.params.paymentId}`).update(update);
+            }
+
         } catch (error) {
             console.log(error);
         }
